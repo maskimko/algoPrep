a221e36c9cd0879a4b515b31e2c538f6d0ec34a2
Maksym Shkolnyi <maksymsh@wix.com>
2022-01-21 13:53:08 +0200

    proper implementation (Backtrack)

--- src/permutations/Solution.java
+++ src/permutations/Solution.java
@@ -6,7 +6,26 @@ import java.util.stream.Collectors;
 class Solution {
 
     public static List<List<Character>> permute(char[] letters) {
-       return permuteChars(letters);
+      List<List<Character>> result = new LinkedList<>();
+      List<Character> path = new LinkedList<>();
+      boolean[] used = new boolean[letters.length];
+      permuteBacktrack(path,used,result,letters);
+      return result;
+    }
+
+    private static void permuteBacktrack(List<Character> path, boolean[] used, List<List<Character>> result, char[]letter){
+        if (path.size() == used.length) {
+            result.add(new LinkedList<>(path));
+            return;
+        }
+        for (int i =0; i< letter.length; i++){
+            if (used[i]) continue;
+            path.add(letter[i]);
+            used[i] = true;
+            permuteBacktrack(path,used, result, letter);
+            path.remove(path.size()-1);
+            used[i] = false;
+        }
     }
 
     private static List<List<Character>> permuteChars(char[] letters){
