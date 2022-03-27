8d1af508aaf6e3cbe78b9b347bee6e5e75348ecf
Maksym Shkolnyi <maksymsh@wix.com>
2022-03-27 19:39:11 +0200

    alienDictionary correct

--- src/alienDictionary/solution.go
+++ src/alienDictionary/solution.go
@@ -2,10 +2,37 @@ package main
 
 import (
 	"bufio"
+	"container/heap"
 	"fmt"
 	"os"
 	"strings"
 )
+
+type pQueue []rune
+
+func (p pQueue) Len() int {
+	return len(p)
+}
+
+func (p pQueue) Less(i, j int) bool {
+	return p[i] < p[j]
+}
+
+func (p pQueue) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+func (p *pQueue) Push(x interface{}) {
+	*p = append(*p, x.(rune))
+}
+
+func (p *pQueue) Pop() interface{} {
+	old := *p
+	n := len(old)
+	x := old[n-1]
+	*p = old[0 : n-1]
+	return x
+}
 
 func countParents(graph map[rune][]rune) map[rune]int {
 	counts := make(map[rune]int)
@@ -24,15 +51,23 @@ func countParents(graph map[rune][]rune) map[rune]int {
 
 func makeGraph(words []string) map[rune][]rune {
 	graph := make(map[rune][]rune)
+	if len(words) == 0 {
+		return nil
+	}
 	for _, w := range words {
-		runes := []rune(w)
-		for i := 1; i < len(runes); i++ {
-			if runes[i-1] != runes[i] {
-				graph[runes[i-1]] = append(graph[runes[i-1]], runes[i])
+		for _, r := range []rune(w) {
+			graph[r] = nil
+		}
+	}
+	for n := 1; n < len(words); n++ {
+		bigger := []rune(words[n-1])
+		smaller := []rune(words[n])
+		for i := 0; i < len(bigger) && i < len(smaller); i++ {
+			if bigger[i] == smaller[i] {
+				continue
 			}
-			if _, ok := graph[runes[i]]; !ok {
-				graph[runes[i]] = nil
-			}
+			graph[bigger[i]] = append(graph[bigger[i]], smaller[i])
+			break
 		}
 	}
 	return graph
@@ -42,20 +77,18 @@ func alienOrder(words []string) string {
 	graph := makeGraph(words)
 	parCount := countParents(graph)
 	visited := make(map[rune]bool)
-	queue := make([]rune, 0)
+	pq := &pQueue{}
+	heap.Init(pq)
 	var result []rune
-	for r := range parCount {
-		if parCount[r] == 0 {
-			queue = append(queue, r)
+	for t := 0; pq.Len() == 0; t++ {
+		for r := range parCount {
+			if parCount[r] == t {
+				heap.Push(pq, r)
+			}
 		}
 	}
-	for len(queue) > 0 {
-		//if len(queue)!=1{
-		//	// undetermined order
-		//	return ""
-		//}
-		c := queue[0]
-		queue = queue[1:]
+	for pq.Len() > 0 {
+		c := heap.Pop(pq).(rune)
 		if visited[c] {
 			continue
 		}
@@ -64,9 +97,12 @@ func alienOrder(words []string) string {
 		for i := 0; i < len(graph[c]); i++ {
 			parCount[graph[c][i]] -= 1
 			if parCount[graph[c][i]] == 0 {
-				queue = append(queue, graph[c][i])
+				heap.Push(pq, graph[c][i])
 			}
 		}
+	}
+	if len(result) < len(graph) {
+		return ""
 	}
 	return string(result)
 }
--- src/alienDictionary/solution_test.go
+++ src/alienDictionary/solution_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"container/heap"
+	"reflect"
+	"strings"
+	"testing"
+)
 
 func Test_alienOrder(t *testing.T) {
 	type args struct {
@@ -14,11 +19,49 @@ func Test_alienOrder(t *testing.T) {
 		{name: "case 1",
 			args: args{words: []string{"wrt", "wrf", "er", "ett", "rftt"}},
 			want: "wertf"},
+		{name: "case 3",
+			args: args{words: []string{"she", "sell", "seashell", "seashore", "seahorse", "on", "a"}},
+			want: "lnrsheoa"},
+		{name: "case 5",
+			args: args{words: strings.Split("da la na fa fei jia ha hai hang hua ta sha shi si ba", " ")},
+			want: ""},
+		{name: "simple 1 char",
+			args: args{words: []string{"e", "d", "c", "b", "a"}},
+			want: "edcba"},
+		{name: "simple 2 char",
+			args: args{words: []string{"ed", "ec", "db", "da", "ce", "cb", "ca", "ac", "ab"}},
+			want: "edcba"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := alienOrder(tt.args.words); got != tt.want {
 				t.Errorf("alienOrder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_pQueue_Pop(t *testing.T) {
+	tests := []struct {
+		name string
+		p    pQueue
+		want []rune
+	}{
+		{name: "PQ test int",
+			p:    pQueue{3, 2, 5},
+			want: []rune{rune(1), rune(2), rune(3), rune(4), rune(5)}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			heap.Init(&tt.p)
+			heap.Push(&tt.p, rune(1))
+			heap.Push(&tt.p, rune(4))
+			got := make([]rune, 0)
+			for tt.p.Len() > 0 {
+				got = append(got, heap.Pop(&tt.p).(rune))
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("pq = %v, want %v", got, tt.want)
 			}
 		})
 	}
