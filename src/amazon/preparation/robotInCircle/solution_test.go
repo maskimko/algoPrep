7da35c276fc54c3e4d943bbfe3e0b546b9257259
Maksym Shkolnyi <maksymsh@wix.com>
2022-04-11 13:56:13 +0200

    robot in circle

---
+++ src/amazon/preparation/robotInCircle/solution_test.go
@@ -0,0 +1,25 @@
+package robotInCircle
+
+import "testing"
+
+func Test_isRobotBounded(t *testing.T) {
+	type args struct {
+		instructions string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "from description",
+			args: args{instructions: "GGLLGG"},
+			want: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isRobotBounded(tt.args.instructions); got != tt.want {
+				t.Errorf("isRobotBounded() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
