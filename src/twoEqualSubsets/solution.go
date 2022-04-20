1104978c8501e82dbdb3c7ef651b97770303266e
Maksym Shkolnyi <maksymsh@wix.com>
2022-04-20 10:56:22 +0200

    two equal subsets top down DFS

---
+++ src/twoEqualSubsets/solution.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+top to bottom DFS solution
+*/
+
+func dfs(nums []int, target, mask int, memo map[int]int) bool {
+
+	if res, ok := memo[mask]; ok {
+		return res == target
+	}
+	if target == 0 {
+		return true
+	}
+	sum := 0
+	for i := 0; i < len(nums); i++ {
+		if mask&(1<<i) == 0 {
+			continue
+		}
+		sum += nums[i]
+	}
+	memo[mask] = sum
+	for i := 0; i < len(nums); i++ {
+		if mask&(1<<i) == 0 {
+			continue
+		}
+		m := mask & ^(1 << i)
+		t := target - nums[i]
+		ok := dfs(nums, t, m, memo)
+		if ok {
+			return ok
+		}
+	}
+	return false
+}
+
+func canPartition(nums []int) bool {
+	mask := 1<<len(nums) - 1
+	memo := make(map[int]int)
+	sum := 0
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+	}
+	if sum%2 == 1 {
+		return false
+	}
+	return dfs(nums, sum/2, mask, memo)
+}
+
+func arrayAtoi(arr []string) []int {
+	res := []int{}
+	for _, x := range arr {
+		v, _ := strconv.Atoi(x)
+		res = append(res, v)
+	}
+	return res
+}
+
+func splitWords(s string) []string {
+	if s == "" {
+		return []string{}
+	}
+	return strings.Split(s, " ")
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	nums := arrayAtoi(splitWords(scanner.Text()))
+	res := canPartition(nums)
+	fmt.Println(res)
+}
---
+++ src/twoEqualSubsets/solution_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func Test_canPartition(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "case 1",
+			args: args{[]int{3, 4, 7}},
+			want: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canPartition(tt.args.nums); got != tt.want {
+				t.Errorf("canPartition() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
